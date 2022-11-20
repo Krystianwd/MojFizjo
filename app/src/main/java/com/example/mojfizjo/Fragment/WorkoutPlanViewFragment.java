e31b168a3e2e3bc8b674f05c65030b3e25271de7
PiotrGajek <piotr.gajek@pollub.edu.pl>
2022-11-20 18:02:00 +0100

    Po wykonaniu planu jest on odznaczany na liście

--- app/src/main/java/com/example/mojfizjo/Fragment/WorkoutPlanViewFragment.java
+++ app/src/main/java/com/example/mojfizjo/Fragment/WorkoutPlanViewFragment.java
@@ -16,12 +16,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mojfizjo.Adapters.WorkoutPlanViewFragmentRecyclerViewAdapter;
+import com.example.mojfizjo.MainActivity;
 import com.example.mojfizjo.Models.ExerciseModel;
+import com.example.mojfizjo.Models.PlanModel;
 import com.example.mojfizjo.R;
+import com.example.mojfizjo.UserSettings;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 
 public class WorkoutPlanViewFragment extends Fragment {
+
+    MainActivity mainActivity;
+    UserSettings userSettings;
+    public ArrayList<PlanModel> planModels;
+
     public WorkoutPlanViewFragment() {
         // Required empty public constructor
     }
@@ -31,6 +43,12 @@ public class WorkoutPlanViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mainActivity = (MainActivity) getContext();
+        assert mainActivity != null;
+        planModels = mainActivity.planModels;
+        userSettings = mainActivity.userSettings;
+
         view = inflater.inflate(R.layout.fragment_workout_plan_view, container, false);
         assert getArguments() != null;
 
@@ -40,6 +58,23 @@ public class WorkoutPlanViewFragment extends Fragment {
 
         Button endSessionButton = view.findViewById(R.id.endSessionButton);
         endSessionButton.setOnClickListener(view -> {
+
+            //odznaczenie planu
+            String dayOfTheWeek = new SimpleDateFormat("EEEE").format(new Date());
+            for (int i = 0; i < planModels.size(); i++) {
+                Map<String, Boolean> remindDay = planModels.get(i).getRemindDay();
+                if (remindDay.containsKey(dayOfTheWeek) && Boolean.FALSE.equals(remindDay.get(dayOfTheWeek))) {
+                    PlanModel planModel = planModels.get(i);
+                    if(Objects.equals(planModel.getPlanName(), planName)){
+                        Log.d(TAG, "plan done:" + planModel.getPlanName());
+                        remindDay.put(dayOfTheWeek, true);
+                        planModel.setRemindDay(remindDay);
+                        planModels.set(i,planModel);
+                    }
+                }
+            }
+
+            //przekierowanie na fragment domowy
             HomeFragment fragment = new HomeFragment();
             AppCompatActivity activity = (AppCompatActivity) view.getContext();
             activity.getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).addToBackStack(null).commit();
