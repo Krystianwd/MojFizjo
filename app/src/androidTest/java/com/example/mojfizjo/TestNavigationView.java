897cb592ca5a02b6f7e68b1de31dddc80ab88ac1
Krystianwd <krystian.debiec999@gmail.com>
2022-12-12 20:48:44 +0100

    Dodanie testów dla spinnera oraz dialogu przypomnień

--- app/src/androidTest/java/com/example/mojfizjo/TestNavigationView.java
+++ app/src/androidTest/java/com/example/mojfizjo/TestNavigationView.java
@@ -1,11 +1,19 @@
 package com.example.mojfizjo;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 
 import android.content.Context;
 
@@ -39,4 +47,14 @@ public class TestNavigationView {
         onView(withId(R.id.nav_notification)).perform(click());
         onView(withText("Mój Fizjo")).check(matches(isDisplayed()));
     }
+    @Test
+    public void givenRemindHour_whenClickedApply_thenCloseDialog(){
+        onView(withId(R.id.nav_notification)).perform(click());
+        onView(withId(R.id.remind_hour_before_training)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("00:00"))).perform(click());
+        onView(withId(R.id.remind_hour_before_training)).check(matches(withSpinnerText(containsString("00:00"))));
+        onView(withText("Zatwierdź")).perform(click());
+        onView(withText("Mój Fizjo")).check(matches(not(isDisplayed())));
+    }
+
 }
